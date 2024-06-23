97064a8ff7dfed49cb521c08ce2634362211bfd8
Jayeeta22 <jayeeta97.riya@gmail>
2024-06-23 20:03:38 +0530

    updated

--- src/app/components/HomeSearch.jsx
+++ src/app/components/HomeSearch.jsx
@@ -51,7 +51,7 @@ export default function HomeSearch() {
           className='bg-[#f8f9fa] rounded-md text-sm text-gray-800 hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow disabled:opacity-80 disabled:shadow-sm'
           onClick={randomSearch}
         >
-          {randomSearchLoading ? 'Loading...' : 'I am feeling lucky'}
+          {randomSearchLoading ? 'Loading...' : 'I am feeling happy'}
         </button>
       </div>
     </>
